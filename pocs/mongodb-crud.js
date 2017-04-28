3e07f68445b1a06a504e55a700ff4a594aaea763
aricallen <aric.allen2@gmail.com>
2017-04-27 17:45:16 -0700

    inserting many docs

--- pocs/mongodb-crud.js
+++ pocs/mongodb-crud.js
@@ -8,13 +8,25 @@ const url = 'mongodb://localhost:27017/team_crosswords';
 // Use connect method to connect to the server
 MongoClient.connect(url, function(err, db) {
   const data = { "date": Date.now() };
+  const manyData = [data, data, data];
+  const forceServerObjectId = true;
 
-  db.collection('puzzles').insertOne(data)
+  // insert one
+  db.collection('puzzles').insertOne(data, { forceServerObjectId })
+    // insert many
+    .then(() => {
+      return db.collection('puzzles').insertMany(manyData, { forceServerObjectId });
+    })
+    // find all
     .then(() => {
       return db.collection('puzzles').find().toArray();
     })
-    .then((result) => {
-      console.log(`result:`, result);
+    // display all then delete
+    .then((docs) => {
+      console.log(`docs:`, docs);
+      // docs.forEach((doc) => {
+      //   db.collection('puzzles')
+      // });
     })
     .then(() => {
       db.close();
